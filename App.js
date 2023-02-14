a4c04782fcde27d9569c7d3b3a41b1fc19b7cdaa
philipp <philipp.reiter90@gmail.com>
2023-02-14 09:58:11 +0100

    expo-image crash

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{flex: 1}}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image
+        source={require('./src/assets/favicon.png')}
+        style={{ width: 300, height: 300 }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
